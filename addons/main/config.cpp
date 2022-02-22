1d995af5de29121a7dec83b7d888b456277c6778
johnb432 <58661205+johnb432@users.noreply.github.com>
2022-02-22 18:18:09 +0100

    1.0.0.2

--- addons/main/config.cpp
+++ addons/main/config.cpp
@@ -11,25 +11,9 @@ class CfgPatches {
             "RWR_gear"
         };
         author = "johnb43";
+        authors[] = {"johnb43"};
         url = "https://github.com/johnb432/Weapons-Balance-RWR";
         VERSION_CONFIG;
-    };
-};
-
-class CfgMods {
-    class PREFIX {
-        name = "Weapons Balance - Russian Winter Retextures";
-        author = "johnb43";
-        tooltipOwned = "Weapons Balance - Russian Winter Retextures";
-        hideName = 0;
-        hidePicture = 0;
-        actionName = "Github";
-        action = "https://github.com/johnb432/Weapons-Balance-RWR";
-        description = "A collection of balancing mods made by johnb43.";
-        overview = "A collection of balancing mods made by johnb43.";
-        picture = "\z\wb_rwr\addons\main\ui\logo_weapons_balance.paa"; // http://getdrawings.com/get-drawing#gun-drawing-in-pencil-27.jpg, http://getdrawings.com/get-drawing#gun-drawing-in-pencil-17.png
-        logo = "\z\wb_rwr\addons\main\ui\logo_weapons_balance.paa";
-        overviewPicture = "\z\wb_rwr\addons\main\ui\logo_weapons_balance.paa";
     };
 };
 
